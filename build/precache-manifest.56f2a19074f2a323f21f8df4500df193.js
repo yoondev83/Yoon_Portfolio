9600b61b63cfacdcad37974f3e4cccacc3eecf53
yoondev83 <yoondev83@gmail.com>
2021-09-30 21:34:39 +0900

    first commit

---
+++ build/precache-manifest.56f2a19074f2a323f21f8df4500df193.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57fdf599edecdedc86f1e3888d72bdc7",
+    "url": "/Yoon_Portfolio/index.html"
+  },
+  {
+    "revision": "5af8143a872bdb90cf13",
+    "url": "/Yoon_Portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "31fc33959a8fb202b8d7",
+    "url": "/Yoon_Portfolio/static/css/main.db520bf6.chunk.css"
+  },
+  {
+    "revision": "5af8143a872bdb90cf13",
+    "url": "/Yoon_Portfolio/static/js/2.764ec5c7.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Yoon_Portfolio/static/js/2.764ec5c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31fc33959a8fb202b8d7",
+    "url": "/Yoon_Portfolio/static/js/main.783fbef7.chunk.js"
+  },
+  {
+    "revision": "5aa5f3cf93412373bac5",
+    "url": "/Yoon_Portfolio/static/js/runtime-main.e0c8455b.js"
+  }
+]);
